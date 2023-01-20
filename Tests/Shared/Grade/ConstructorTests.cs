be3b0388b51e609cc9be795e29e1cc63469baf7c
TakenouchiTR <62298020+TakenouchiTR@users.noreply.github.com>
2023-01-20 10:26:10 -0500

    Add tests for the Grade constructor
    Adds tests for the Grade's constructor, including null values for all applicable parameters.

---
+++ Tests/Shared/Grade/ConstructorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapstoneDemo.Shared;
+
+namespace Tests.Shared.Grade
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [TestCase(100, "Thomas", "Math", 92.5)]
+        [TestCase(null, "Thomas", "Math", 92.5)]
+        [TestCase(100, null, "Math", 92.5)]
+        [TestCase(100, "Thomas", null, 92.5)]
+        [TestCase(100, "Thomas", "Math", -1)]
+        public void CreateValidGrade(int? id, string name, string subject, double gradeAmount)
+        {
+            CapstoneDemo.Shared.Grade grade = new CapstoneDemo.Shared.Grade(id, name, subject, gradeAmount);
+            Assert.Multiple(() =>
+            {
+                Assert.That(grade.Id, Is.EqualTo(id));
+                Assert.That(grade.Name, Is.EqualTo(name));
+                Assert.That(grade.Subject, Is.EqualTo(subject));
+                Assert.That(grade.GradeAmount, Is.EqualTo(gradeAmount));
+            });
+        }
+    }
+}
