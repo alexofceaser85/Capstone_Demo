b3802023cf48b793c5100265efa271c0c02584ad
Alexander Ayers <alexayers2000@gmail.com>
2023-01-21 17:52:12 -0500

    Added infrastructure to get grade, temporaily using a value in memory.

---
+++ BlazorWinForms/Services/GradesService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapstoneDemo.Shared;
+
+namespace BlazorWinForms.Services
+{
+    public class GradesService : IEnumerable<Grade>
+    {
+        private List<Grade> grades;
+
+        public GradesService()
+        {
+            this.grades = new List<Grade> {
+                new(null, "John", "Math", 100),
+                new(null, "John", "Science", 87),
+                new(null, "John", "English", 75),
+                new(null, "John", "History", 95),
+                new(null, "John", "Art", 90),
+                new(null, "John", "Music", 60),
+                new(null, "John", "PE", 100),
+                new(null, "John", "Computer Science", 80)
+            };
+        }
+
+        public List<Grade> GetGrades()
+        {
+            return this.grades;
+        }
+
+        public void AddGrade(string name, string subject, int amount)
+        {
+           this.grades.Add(new Grade(null, name, subject, amount));
+        }
+
+        public IEnumerator<Grade> GetEnumerator()
+        {
+            return this.grades.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ BlazorWinForms/ViewModel/GradesViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorWinForms.Services;
+using CapstoneDemo.Shared;
+
+namespace BlazorWinForms.ViewModel
+{
+    public class GradesViewModel : IEnumerable<Grade>, INotifyCollectionChanged
+    {
+        private readonly GradesService gradesService;
+
+        public event NotifyCollectionChangedEventHandler? CollectionChanged;
+
+        public GradesViewModel()
+        {
+            this.gradesService = new GradesService();
+        }
+
+        public ObservableCollection<Grade> GetGrades()
+        {
+            var newGrades = this.gradesService.GetGrades();
+            this.CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+            return new ObservableCollection<Grade>(newGrades);
+        }
+
+        public void AddGrade(string name, string subject, int amount)
+        {
+            this.gradesService.AddGrade(name, subject, amount);
+            this.GetGrades();
+        }
+
+        public IEnumerator<Grade> GetEnumerator()
+        {
+            return this.gradesService.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
