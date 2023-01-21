75659c8adbe5fe79f9be4f05ed57f18910347335
Alexander Ayers <alexayers2000@gmail.com>
2023-01-21 15:57:30 -0500

    Added initial Blazor desktop application.

---
+++ BlazorWinForms/Form1.cs
@@ -0,0 +1,31 @@
+﻿using BlazorWinForms.Data;
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlazorWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var services = new ServiceCollection();
+            services.AddWindowsFormsBlazorWebView();
+            services.AddSingleton<WeatherForecastService>();
+            blazorWebView1.HostPage = "wwwroot\\index.html";
+            blazorWebView1.Services = services.BuildServiceProvider();
+            blazorWebView1.RootComponents.Add<Main>("#app");
+        }
+
+    }
+}
