73673ff6ff95cbc333ccaf3889dea55d2e8177f6
TakenouchiTR <62298020+TakenouchiTR@users.noreply.github.com>
2023-01-24 20:27:30 -0500

    Add tests for BlazorWinForms project
    Add tests for all items in the BlazorWinForms project.

---
+++ Tests/BlazorWinForms/Endpoints/GradesEndpoints/AddGradeTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapstoneDemo.Shared;
+using Server.Dal;
+
+namespace Tests.BlazorWinForms.Endpoints.GradesEndpoints
+{
+    [TestFixture]
+    public class AddGradeTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            GradesDal.RemoveGrades();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            GradesDal.RemoveGrades();
+        }
+        
+        [TestCase("Alpha", "Math", 90)]
+        [TestCase("Beta", "Computer Science", 20)]
+        [TestCase("Charlie", "Physics", 80)]
+        public void AddGrade(string name, string course, double gradeAmount)
+        {
+            Grade grade = new Grade(null, name, course, gradeAmount);
+            global::BlazorWinForms.Endpoints.GradeEndpoints.AddGrade(grade);
+
+            Grade[] grades = global::WinFormsClient.Endpoints.GradesEndpoints.GetGrades();
+            Assert.Multiple( () => {
+                    Assert.That(grades.Length, Is.EqualTo(1));
+                    Assert.That(grades[0].Name, Is.EqualTo(grade.Name));
+                    Assert.That(grades[0].Subject, Is.EqualTo(grade.Subject));
+                    Assert.That(grades[0].GradeAmount, Is.EqualTo(grade.GradeAmount));
+                }
+            );
+            
+        }
+
+        [Test]
+        public void AddManyGrade()
+        {
+            Grade[] grades = {
+                new Grade(null, "Alpha", "Math", 90),
+                new Grade(null, "Beta", "Computer Science", 90),
+                new Grade(null, "Charlie", "Physics", 90),
+            };
+
+            foreach (Grade grade in grades)
+            {
+                global::BlazorWinForms.Endpoints.GradeEndpoints.AddGrade(grade);
+            }
+
+            Grade[] retrievedGrades = global::BlazorWinForms.Endpoints.GradeEndpoints.GetGrades();
+            Assert.Multiple(() => {
+                    Assert.That(retrievedGrades.Length, Is.EqualTo(grades.Length));
+                    for (int i = 0; i < retrievedGrades.Length; i++)
+                    {
+                        Assert.That(retrievedGrades[i].Name, Is.EqualTo(grades[i].Name));
+                        Assert.That(retrievedGrades[i].Subject, Is.EqualTo(grades[i].Subject));
+                        Assert.That(retrievedGrades[i].GradeAmount, Is.EqualTo(grades[i].GradeAmount));
+                    }
+
+                }
+            );
+        }
+    }
+}
---
+++ Tests/BlazorWinForms/ViewModel/GradesViewModel/GetEnumeratorTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapstoneDemo.Shared;
+using Server.Dal;
+
+namespace Tests.BlazorWinForms.ViewModel.GradesViewModel
+{
+    [TestFixture]
+    public class GetEnumeratorTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            GradesDal.RemoveGrades();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            GradesDal.RemoveGrades();
+        }
+
+        [Test]
+        public void GetEmptyEnumerator()
+        {
+            global::BlazorWinForms.ViewModel.GradesViewModel viewModel = new();
+            using var enumerator = viewModel.GetEnumerator();
+
+            Assert.That(enumerator.Current, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void GetEmptyGenericEnumerator()
+        {
+            global::BlazorWinForms.ViewModel.GradesViewModel viewModel = new();
+            IEnumerable enumerator = viewModel;
+            Assert.That(enumerator.GetEnumerator(), !Is.EqualTo(null));
+        }
+    }
+}
